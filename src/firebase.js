db4537705135ab1b01f06baebf7c022096474d4c
Leonard Ata <atatangban@live.com>
2021-07-18 01:17:20 +0100

    Firebase initialization

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAutrOAfPrhsiZVR_EAiZMoNJYlGmsHmsE",
+  authDomain: "expressdotcom-8a08a.firebaseapp.com",
+  projectId: "expressdotcom-8a08a",
+  storageBucket: "expressdotcom-8a08a.appspot.com",
+  messagingSenderId: "50498906195",
+  appId: "1:50498906195:web:29a4c3e15dd05a1171bea6",
+  measurementId: "G-VKRW6XYHFZ"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
