97525cffd70fb439ff2cc74bfb409079ed067092
Leonard Ata <atatangban@live.com>
2021-07-22 01:47:08 +0100

    header design

---
+++ src/Header.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search'
+import PersonIcon from '@material-ui/icons/Person'
+import LocalMallIcon from '@material-ui/icons/LocalMall'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <span className='header_message'>Free Shipping on orders above $50</span>
+      <img 
+        className='header_logo'
+        src='https://www.express.com/content/dam/logos-fonts/logos/express/express-logo.svg' 
+        alt=''
+      />      
+      <div className='header_icons'>
+        <SearchIcon className='header_searchIcon' />
+        <PersonIcon className='header_personIcon' />
+        <LocalMallIcon className='header_localMallIcon' />
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Nav.css'
+
+const Nav = () => {
+  return (
+    <div className='nav'>
+      <a href=''>WOMEN</a>
+      <a href=''>MEN</a>
+      <a href=''>JEANS</a>
+      <a href=''>NEW</a>
+      <a href=''>SALE</a>
+      <a href=''>STYLE TRIAL RENTAL</a>
+      <a href=''>SERVICES</a>
+      <a href=''>LABELS WE LOVE</a>
+      <a href=''>EXPRESS OUTLET</a>
+      
+    </div>
+  )
+}
+
+export default Nav
